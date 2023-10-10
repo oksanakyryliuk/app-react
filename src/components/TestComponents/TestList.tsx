bf0fa703b0ba49491d5c1c8fabec504ab0fb07bb
Valeriia Chyrko <valeriia.chyrko@oa.edu.ua>
2023-10-10 23:05:43 +0300

    Separated Text From Modal file to independent components.

--- src/components/TestComponents/TestList.tsx
+++ src/components/TestComponents/TestList.tsx
@@ -7,7 +7,7 @@ export const TestCardList = ({ tests }: {tests: Array<Test>}) => {
     return (
         <ListItem >
             {tests.map((test, index) => (
-                <TestCard  key={index} title={test.title} />
+                <TestCard  key={index} title={test.Title} />
             ))}
         </ListItem>
     );
