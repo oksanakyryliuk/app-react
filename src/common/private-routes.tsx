c7f6e9539ccdc78d4e6fdbc11189c5ed12379304
okyryliuk <okyryluik@gmail.com>
2023-10-29 23:02:12 +0200

    add provate rotes for moderator, user

--- src/common/private-routes.tsx
+++ src/common/private-routes.tsx
@@ -1,9 +1,20 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import { AppModules } from './../enums/AppModules';
+import { AppModules } from '../common/enums/AppModules';
 import { useAuth } from '../auth/hooks/useAuth';
 
 export const PrivateRoutesAdmin = () => {
   const { isLoggedIn, isAdmin } = useAuth();
   console.log(isAdmin)
-  return isLoggedIn &&isAdmin ? <Outlet /> : <Navigate to={AppModules.Main} />;
+  return isLoggedIn &&isAdmin ? <Outlet /> : <Navigate to={AppModules.Admin} />;
+};
+
+export const PrivateRoutesUser = () => {
+  const { isLoggedIn, isAdmin } = useAuth();
+  console.log(isAdmin)
+  return isLoggedIn &&isAdmin ? <Outlet /> : <Navigate to={AppModules.User} />;
+};
+export const PrivateRoutesModerator = () => {
+  const { isLoggedIn, isModerator } = useAuth();
+  console.log(isModerator)
+  return isLoggedIn &&isModerator ? <Outlet /> : <Navigate to={AppModules.Moderator} />;
 };
