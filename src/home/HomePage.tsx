7add64be34356c90b906f339f1400e84877aa02f
okyryliuk <okyryluik@gmail.com>
2023-10-29 23:02:53 +0200

    add logic on login (pages Admin, User, Moderator)

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -3,21 +3,32 @@ import Box from '@mui/material/Box';
 import { useAuth } from '../auth/hooks/useAuth';
 import { Link } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
-import { AppModules } from '../enums/AppModules';
+import { AppModules } from '../common/enums/AppModules';
+import { useNavigate } from 'react-router-dom';
 
 export const HomePage = () => {
-  const { isLoggedIn } = useAuth();
-  return (
-    <Box>
-      {isLoggedIn ? (
-        <Link component={RouterLink} to={AppModules.Main}>
-          Dashboard
-        </Link>
-      ) : (
-        <Link component={RouterLink} to={AppModules.Login}>
-          Login
-        </Link>
-      )}
-    </Box>
-  );
+    const { isLoggedIn, isAdmin, isUser, isModerator } = useAuth(); // Assuming you have access to user roles
+    const navigate = useNavigate();
+    let redirectTo = AppModules.Login;
+    if (isLoggedIn) {
+        if (isAdmin) {
+            redirectTo = AppModules.Admin;
+        } else if (isUser) {
+            redirectTo = AppModules.User;
+        } else if (isModerator) {
+            redirectTo = AppModules.Moderator;
+        }
+    } else {
+        redirectTo = AppModules.Login;
+    }
+    const handleClick = () => {
+        navigate(redirectTo); // Perform the redirection
+    };
+    return (
+        <Box>
+            <button onClick={handleClick}>
+                {isLoggedIn ? 'Dashboard' : 'Login'}
+            </button>
+        </Box>
+    );
 };
