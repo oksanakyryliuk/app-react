f877096ce1f9007e29136f348602142f3276e2dd
okyryliuk <okyryluik@gmail.com>
2023-11-02 20:59:19 +0200

    del moderator

--- src/common/private-routes.tsx
+++ src/common/private-routes.tsx
@@ -10,11 +10,5 @@ export const PrivateRoutesAdmin = () => {
 
 export const PrivateRoutesUser = () => {
   const { isLoggedIn, isAdmin } = useAuth();
-  console.log(isAdmin)
   return isLoggedIn &&isAdmin ? <Outlet /> : <Navigate to={AppModules.User} />;
-};
-export const PrivateRoutesModerator = () => {
-  const { isLoggedIn, isModerator } = useAuth();
-  console.log(isModerator)
-  return isLoggedIn &&isModerator ? <Outlet /> : <Navigate to={AppModules.Moderator} />;
 };
